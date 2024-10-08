5f0375862437b14f92c52c9aa8143d75cee4d4fc
carlos123asd <carlos-medin@hotmail.com>
2024-10-08 23:12:37 +0200

    ADD: Index 5(Resolved)

---
+++ index5.php
@@ -0,0 +1,45 @@
+<?php
+    /*
+        index5.php - Conectar a la base de datos de MySQL utilizando mysqli. Hacer
+        una consulta para obtener las habitaciones y mostrarlas abajo utilizando el
+        código de index3.php
+    */
+    $mysql_connection = new mysqli("localhost","root","123456789","hoteldb");
+    if($mysql_connection -> connect_errno){
+        echo "Failed Connection: " . $mysql_connection -> connect_errno;
+        exit();
+    }
+
+    $query = "SELECT
+                r.idRoom,
+                r.roomNumber,
+                r.typeRoom,
+                r.description,
+                r.offer,
+                r.price,
+                r.discount,
+                r.cancellation,
+                r.status,
+                GROUP_CONCAT(DISTINCT p.uri ORDER BY p.uri SEPARATOR ', ') AS photos,
+                GROUP_CONCAT(DISTINCT a.name ORDER BY a.name SEPARATOR ', ') AS amenities
+            FROM
+                rooms r
+            LEFT JOIN
+                photosRoom p ON r.idRoom = p.idRoom
+            LEFT JOIN
+                amenities_rooms ar ON r.idRoom = ar.idRoom
+            LEFT JOIN
+                amenities a ON ar.idAmenitie = a.idAmenitie
+            GROUP BY
+                r.idRoom;";
+
+    $result = $mysql_connection -> query($query);
+    $data = $result -> fetch_assoc();
+    echo "<ol>";
+        echo "<li>";
+            foreach($data as $key => $value){
+                echo "$key: $value <br>";
+            }
+        echo "</li>";
+    echo "</ol>";
+?>
